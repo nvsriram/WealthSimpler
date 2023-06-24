4e5357b2af5ef8880f277b9ab51500f9100857f0
Vijay <sccr247@gmail.com>
2023-06-24 10:39:29 -0400

    login page

---
+++ frontend/src/Login.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.css";
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <h1>WealthSimpler</h1>
+      <div className="form-container">
+        <input type="text" placeholder="Username" className="input-field" />
+        <input type="password" placeholder="Password" className="input-field" />
+        <button className="login-button">Login</button>
+        <button className="signup-button">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
