47a7824b829bb773bd71fa25b749bcdef9b3de9c
Shiv Gupta <shivg56@gmail.com>
2023-06-24 22:01:21 -0400

    setting up firebase and storing the public/private key upon login based on email

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { Route, Routes } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Login from "./pages/Login/Login";
 import "./styles.css";
+
+import {
+  getEmailKeys,
+  checkEmailExists,
+  addFieldsToDatabase,
+} from "./middleware/store";
 
 import { createAccount } from "./middleware/createAccount";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -15,9 +21,37 @@ import Gas from "./pages/Gas/Gas";
 
 const App = () => {
   const { user, isAuthenticated } = useAuth0();
+  const [publicKey, setPublicKey] = useState<string | null>(null);
+  const [privateKey, setPrivateKey] = useState<string | null>(null);
 
-  console.log("User", user);
-  console.log(createAccount());
+  // console.log("User", user);
+  // console.log(createAccount());
+
+  useEffect(() => {
+    const handleUserData = async () => {
+      if (user && user.email) {
+        try {
+          const emailExists = await checkEmailExists(user.email);
+          if (emailExists) {
+            const { publicKey, privateKey } = await getEmailKeys(user.email);
+            console.log("pubKey", publicKey);
+            console.log("priKey", privateKey);
+            setPublicKey(publicKey);
+            setPrivateKey(privateKey);
+          } else {
+            const [pubKey, privKey] = await createAccount();
+            setPublicKey(pubKey);
+            setPrivateKey(privKey);
+            await addFieldsToDatabase(user.email, pubKey, privKey);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    };
+
+    handleUserData();
+  }, []);
 
   return (
     <div className="app">
