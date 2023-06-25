166f3b80712a074dbede5f6c872642e3944168b4
nvsriram <nuranisri01@gmail.com>
2023-06-24 22:03:17 -0400

    Updated Users

--- frontend/src/pages/Users/Users.tsx
+++ frontend/src/pages/Users/Users.tsx
@@ -1,18 +1,20 @@
-import { Dispatch, SetStateAction, useState } from "react";
-import "./Users.css";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
+import "./Users.css";
+
+type User = {
+  name: string;
+  email: string;
+  phone: string;
+};
 
 const Users = ({
   updateStep,
 }: {
   updateStep: Dispatch<SetStateAction<number>>;
 }) => {
-  const [users, setUsers] = useState([
-    { name: "John Doe", email: "john@example.com", phone: "1234567890" },
-    { name: "Jane Smith", email: "jane@example.com", phone: "9876543210" },
-    // Add more user objects as needed
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
 
   const [newUser, setNewUser] = useState({
     name: "",
@@ -20,10 +22,11 @@ const Users = ({
     phone: "",
   });
 
-  const handleAddUser = () => {
+  const handleAddUser = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (newUser.name !== "" && newUser.email !== "" && newUser.phone !== "") {
       setUsers((prevUsers) => [...prevUsers, newUser]);
-      setNewUser({ name: "", email: "", phone: "" }); // Reset newUser state
+      setNewUser({ name: "", email: "", phone: "" });
     }
   };
 
@@ -34,7 +37,7 @@ const Users = ({
   };
 
   return (
-    <div className="container">
+    <>
       <h1>Add Users</h1>
       <div className="scroll-view">
         <ul>
@@ -45,8 +48,7 @@ const Users = ({
           ))}
         </ul>
       </div>
-      <br />
-      <div className="add-user-container">
+      <form className="add-user-container" onSubmit={handleAddUser}>
         <input
           type="text"
           placeholder="Name"
@@ -65,19 +67,17 @@ const Users = ({
           value={newUser.phone}
           onChange={(e) => setNewUser({ ...newUser, phone: e.target.value })}
         />
-        <AwesomeButton type="danger" onPress={handleAddUser}>
-          +
-        </AwesomeButton>
-      </div>
+        <AwesomeButton type="danger">+</AwesomeButton>
+      </form>
       <AwesomeButton
         type="primary"
         className={`submit-button ${users.length === 0 ? "disabled" : ""}`}
         onPress={handleSubmit}
         disabled={users.length === 0}
       >
-        Submit
+        Continue
       </AwesomeButton>
-    </div>
+    </>
   );
 };
 
